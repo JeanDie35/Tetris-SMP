8b47df86626a4eee2e6231df885dfa3ba7d20ded
Yoan <yoan@example.com>
2026-06-08 09:41:34 +0200

    Deleted test.py

--- game/test.py
+++
@@ -1,9 +0,0 @@
-def divide(x, y):
-    try:
-        result = x / y
-    except Exception as e:
-        print(e)
-    else:
-        print("result is", result)
-    finally:
-        print("executing finally clause")
